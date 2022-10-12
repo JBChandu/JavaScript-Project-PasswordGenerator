99272b00a44b1768af41fc9a41c14bbf184f4ee6
chcodes <chandrahassan15@gmail.com>
2022-10-12 21:26:01 +0530

    Initial Commit

---
+++ ReadMe.md
@@ -0,0 +1,21 @@
+# JavaScript Project - Password Generator
+
+## **Project: Password Generator**
+
+![Badge](https://img.shields.io/badge/Javascript-RandomPasswordGenerator-brightgreen "Random Password Generator")
+
+![Badge with LCO and ineuron tagged](https://img.shields.io/badge/Ineuron.ai-LCO-brightgreen)
+
+![Badge with LCO and ineuron tagged](https://img.shields.io/badge/Full%20Stack%20JavaScript%20bootcamp-Hitesh%20Choudhary-brightgreen)
+
+### Concepts learned from this project:
+- Learned About Generating random password based on the checkboxes selected.
+- Learned about how Navigator is used to copy the password to the clipboard.
+- Little complex one. Enjoyed while doing this.
+
+
+
+**Live Link :** [Deployed Link](https://js-project-passwordgenerator.netlify.app/)
+>### Preview :
+
+![Homepage screenshot](preview.png)
---
+++ script.js
@@ -0,0 +1,101 @@
+const resultEl = document.getElementById("result");
+const lengthEl = document.getElementById("length");
+const uppercaseEl = document.getElementById("uppercase");
+const lowercaseEl = document.getElementById("lowercase");
+const numbersEl = document.getElementById("numbers");
+const symbolsEl = document.getElementById("symbols");
+const generateEl = document.getElementById("generate");
+const clipboardEl = document.getElementById("clipboard");
+
+const randomFunc = {
+  lower: getRandomLower,
+  upper: getRandomUpper,
+  number: getRandomNumber,
+  symbol: getRandomSymbol
+};
+
+clipboardEl.addEventListener("click", (e) => {
+  e.preventDefault();
+  let randPass = navigator.clipboard.writeText(resultEl.innerText);
+  alert("password Copied to clipboard");
+  resultEl.textContent = " ";
+});
+
+generateEl.addEventListener("click", () => {
+  let len = lengthEl.value;
+  // for input length
+  if (len === "" || len <= 4 || len > 20) {
+    alert("Enter or select some value between 4 and 20");
+  }
+  // for check boxes
+  if (uppercaseEl.checked === true) {
+    var up = randomFunc.upper(len);
+  }
+  if (lowercaseEl.checked === true) {
+    var low = randomFunc.lower(len);
+  }
+  if (numbersEl.checked === true) {
+    var numb = randomFunc.number(len);
+  }
+  if (symbolsEl.checked === true) {
+    var sym = randomFunc.symbol(len);
+  }
+
+  if (
+    symbolsEl.checked !== true &&
+    uppercaseEl.checked !== true &&
+    lowercaseEl.checked !== true &&
+    numbersEl.checked !== true
+  ) {
+    alert("please check atleast one checkbox");
+  }
+
+  resultEl.innerText = generatePassword(low, up, numb, sym, len);
+});
+
+function generatePassword(lower, upper, number, symbol, length) {
+  let newWord = lower + upper + number + symbol;
+  let result = "";
+  for (let i = 0; i < length; i++) {
+    result += newWord[Math.ceil(Math.random() * newWord.length - 1)];
+  }
+  return result;
+}
+
+function getRandomLower(num) {
+  let lowerLetters = "abcdefghijklmnopqrstuvwxyz";
+  let resLower = "";
+  for (let i = 0; i < num; i++) {
+    resLower +=
+      lowerLetters[Math.ceil(Math.random() * lowerLetters.length - 1)];
+  }
+  return resLower;
+}
+
+function getRandomUpper(num) {
+  let upperLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  let resUpper = "";
+  for (let i = 0; i < num; i++) {
+    resUpper +=
+      upperLetters[Math.ceil(Math.random() * upperLetters.length - 1)];
+  }
+  return resUpper;
+}
+
+function getRandomNumber(num) {
+  let numbers = `1234567890`;
+  let newNum = "";
+  for (let i = 0; i < num; i++) {
+    newNum += numbers[Math.ceil(Math.random() * numbers.length - 1)];
+  }
+  return newNum;
+}
+
+function getRandomSymbol(num) {
+  let symbols = `!"#$%&'()*+,-./:;<=>?@{}^_'{|}~ `;
+  let symbl = "";
+  for (let i = 0; i < num; i++) {
+    symbl += symbols[Math.ceil(Math.random() * symbols.length - 1)];
+  }
+  return symbl;
+}
